ac663f4fd6ff36e9d009328cea77bac7cafc49e7
EnzoBernardis2007 <enzobernardis2007@gmail.com>
2025-04-18 10:32:46 -0300

    feat(api): added axios and a hook useApi

---
+++ src/hooks/useApi.ts
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import api from '../services/api'
+
+export const useApi = () => {
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState<string | null>(null)
+
+    const request = async <T,>(method: 'get' | 'post', url: string, data?: any) => {
+        setLoading(true)
+        try {
+        const response = await api[method]<T>(url, data)
+        return response.data
+        } catch (err) {
+        setError('Erro na requisição')
+        throw err
+        } finally {
+        setLoading(false)
+        }
+    }
+
+    return { loading, error, request }
+}
